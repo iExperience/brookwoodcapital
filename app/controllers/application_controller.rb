ae7e56e242412ea4eefd5caa446b4d074b8d6ba8
JoshBroomberg <joshbroomberg@gmail.com>
2016-06-16 17:16:43 +0200

    v1 build done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_action :setup_facebook_events
-  before_action :set_default_course_data
-
-  def setup_facebook_events
-    @fb_events = FacebookEventsService.new
-  end
-
-  # NOTE(rafi): Serves any page using @course_data (which is not just the
-  # courses_controller).
-  def set_default_course_data
-    @course_data = CourseInfoService.get_default_info
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,5 @@ Rails.application.routes.draw do
 
   # About routes
   get "/who-we-are" => "home#about", as: :about
-  get "/learning" => "home#learning", as: :learning
-  get "/interning" => "home#interning", as: :interning
-  get "/exploring" => "home#exploring", as: :exploring
-  get "/parents" => "home#parents", as: :parents
-  get "/pricing" => "home#pricing", as: :pricing
-
-  # Courses routes
-  get "/courses", to: redirect("/courses/product-management")
-  get "/courses/:name" => "courses#show", as: :course,
-    name: /finance|consulting|ux-design|coding-ios|coding-web-dev|coding|product-management|data-science/
-
-  # Misc routes
-  get "/faq" => "home#faq", as: :faq
-  get "/alumni" => "home#alumni", as: :alumni
-  get "/privacy" => "home#privacy_policy", as: :privacy
-
-  # This line is last so that if no other routes are matched, the
-  # course entered as /course_name, as was the old convention, is captured 
-  # and redirected to the right location.
-  get "/coding" => redirect("/courses/coding")
-  get "/consulting" => redirect("/courses/consulting")
-  get "/finance" => redirect("/courses/finance")
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  resources :alumni, only: [:index, :show]
+  get "/investment-process" => "home#investment_process", as: :investment_process
 end
