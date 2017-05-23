015c8852a75c6e70429a97b1e6ce2e2aee1401e5
Seth Fuchs <sethfuchs100@gmail.com>
2017-05-23 16:03:11 +0200

    changes from ix cap to brook cap

--- db/migrate/20150824120904_add_year_to_alumnus.rb
+++
@@ -1,5 +0,0 @@
-class AddYearToAlumnus < ActiveRecord::Migration
-  def change
-    add_column :alumni, :year, :string
-  end
-end
--- db/migrate/20150824123643_add_course_to_alumnus.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseToAlumnus < ActiveRecord::Migration
-  def change
-    add_column :alumni, :course, :string
-  end
-end
